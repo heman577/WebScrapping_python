a6dc9955e35b9935ca4b6fb437eae5fd577a386a
hemu <30374046+heman577@users.noreply.github.com>
2020-10-26 12:25:34 +0530

    Add files via upload

---
+++ age_years.py
@@ -0,0 +1,45 @@
+#x=input('enter the value in years;')
+#y=int(x)
+#z=(y*365)
+#print(z)
+#print('you are ',z,'days old')
+
+
+#user_response=input('enter the integer value:')
+#x=int(user_response)
+#y=x*x -12*x+11
+#print(y)
+
+
+#user_response=int(input('enter the integer value:'))
+#my_list=['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY']
+#print(my_list[user_response])
+
+
+
+#sample_list=[2,10,3,5]
+#average=(sample_list[0]+sample_list[1]+sample_list[2]+sample_list[3]) /4
+#print(average)
+
+
+#x= 10
+#if x > 5 :
+ #    x = x + 5
+#if x < 12 :
+ #    x = x + 5
+#if x == 15 :
+ #   x = x + 5
+  #  print(x)
+
+
+
+my_list = [ 'dog' ,'cat' ,'worm' ,2.3]
+  
+if 'doc' in my_list:
+     my_list[1]= 4
+
+else:
+     my_list[2]= 6
+
+print (my_list[1]),my_list[2])    
+     
